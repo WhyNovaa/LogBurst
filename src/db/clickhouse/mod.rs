9062695d144e07f1dd030d66db640a246862f81d
WhyNovaa <novikov.arsenij@gmail.com>
2026-09-02 23:38:05 +0300

    refactor: increase FLUSH_INTERVAL

--- src/db/clickhouse/mod.rs
+++ src/db/clickhouse/mod.rs
@@ -11,7 +11,7 @@ pub mod structs;
 const CHANNEL_SIZE: usize = 200_000;
 const LIVE_CHANNEL_SIZE: usize = 15;
 const INSERTER_MAX_ROWS: u64 = 50_000;
-const FLUSH_INTERVAL: std::time::Duration = std::time::Duration::from_secs(5);
+const FLUSH_INTERVAL: std::time::Duration = std::time::Duration::from_secs(15);
 
 pub struct ClickHouse {
     client: Client,
